0be6e6be30186f8a14389cd0af2b8bfd146bc53b
iamburqu3 <161051367+iamburqu3@users.noreply.github.com>
2025-05-12 12:21:20 -0300

    jj

--- supabaaseClient.js
+++ supabaaseClient.js
@@ -5,4 +5,4 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 export const supabase = createClient(
   'https://wqrvadycionozczmqgoj.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcnZhZHljaW9ub3pjem1xZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0OTUzOTIsImV4cCI6MjA2MjA3MTM5Mn0.mMVAn7kJQH8bLhijfug2E0lMvpjRkKRE7CC29IFNNt4'
-)
+) 
